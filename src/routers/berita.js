b9d9c43ab75de442cf536cc5e4f565f1fc6c81d2
alvinrahmatmaulana1 <akunyang0012gmail.com>
2023-10-27 09:01:02 +0700

    deploy

--- src/routers/berita.js
+++ src/routers/berita.js
@@ -2,20 +2,28 @@ const router = require('express').Router();
 const { berita } = require('../controllers');
 
 const multer = require('multer')
-const fs = require('fs');
+// const fs = require('fs');
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     // Pastikan direktori ini ada dan dapat ditulis
+//     const uploadDir = 'assets/uploads/';
+//     if (!fs.existsSync(uploadDir)) {
+//       fs.mkdirSync(uploadDir);
+//     }
+//     cb(null, uploadDir);
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, file.originalname);
+//   }
+// });
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    // Pastikan direktori ini ada dan dapat ditulis
-    const uploadDir = 'assets/uploads/';
-    if (!fs.existsSync(uploadDir)) {
-      fs.mkdirSync(uploadDir);
+    destination: (req, file, cb) => {
+      cb(null, 'public/uploads/');
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.originalname);
     }
-    cb(null, uploadDir);
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.originalname);
-  }
-});
+  });
 
 const upload = multer({ storage });
 
