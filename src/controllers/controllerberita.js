c12d19ef8cc7d4690bebeca1f81d00b3fbc9f2e1
alvinrahmatmaulana1 <akunyang0012gmail.com>
2023-10-30 12:12:48 +0700

    deploy

--- src/controllers/controllerberita.js
+++ src/controllers/controllerberita.js
@@ -56,7 +56,7 @@ module.exports = {
                     judul : req.body.judul,
                     deskripsi : req.body.deskripsi,
                     tanggal_terbit : req.body.tanggal_terbit,
-                    gambar: req.file.path
+                    gambar: req.body.gambar
                 }
 
         
