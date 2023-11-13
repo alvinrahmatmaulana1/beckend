84e4b422bb15b488fc0058c130a79dbc82c7fbb8
alvinrahmatmaulana1 <akunyang0012gmail.com>
2023-11-13 09:25:18 +0700

    deploy

--- src/controllers/controllerberita.js
+++ src/controllers/controllerberita.js
@@ -148,52 +148,7 @@ module.exports = {
     // },
 
 
-    addDataBerita  (req, res) {
-        const { judul, deskripsi, tanggal_terbit } = req.body;
-        const file = req.file;
-
-        if (!file) {
-          return res.status(400).send('Tidak ada berkas yang diunggah');
-        }
-
-        const storageRef = bucket.file(file.originalname);
-        const fileStream = storageRef.createWriteStream({
-          metadata: {
-            contentType: file.mimetype, // Menggunakan tipe konten dari req.file.mimetype
-          },
-        });
-
-        fileStream.on('error', (err) => {
-          console.error(err);
-          res.status(500).send('Terjadi kesalahan saat mengunggah gambar');
-        });
-
-        fileStream.on('finish', () => {
-          const gambarUrl = `https://beckend.vercel.app/${bucket.name}/${file.originalname}`;
-          const sql = 'INSERT INTO berita (judul, deskripsi, tanggal_terbit, gambar) VALUES (?, ?, ?, ?)';
-
-          pool.getConnection(function (err, connection) {
-            if (err) {
-              console.log(err);
-              res.status(500).send('Terjadi kesalahan saat menghubungkan ke database');
-              return;
-            }
-
-            connection.query(sql, [judul, deskripsi, tanggal_terbit, gambarUrl], function (err, result) {
-              connection.release();
-
-              if (err) {
-                console.error(err);
-                res.status(500).send('Terjadi kesalahan saat menyimpan data ke database');
-              } else {
-                res.status(200).send('Berhasil mengunggah gambar dan menyimpan data');
-              }
-            });
-          });
-        });
-
-        file.stream.pipe(fileStream);
-    },
+    
 
     editDataBerita(req, res) {
         const id = req.params.id;
